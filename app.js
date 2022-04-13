7c780cd2c3410675deae3dcf94860f9cced976a4
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-04-13 21:12:17 +0200

    feat: added logging and routing

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-const express = require("express");
-require("dotenv").config();
+const express = require('express');
+const morgan = require('morgan');
+const mainRoutes = require('./app/routes/main.routes');
+const {
+  unresolvedPathHandler,
+  defaultErrorHandler,
+} = require('./app/middlewares/error.handlers');
+
+require('dotenv').config();
 
 const app = express();
+
+app.set('trust proxy', 1);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('tiny'));
+
+app.use(mainRoutes);
+app.use(unresolvedPathHandler);
+app.use(defaultErrorHandler);
 
 app.listen(process.env.PORT || 3000, () =>
   console.log(`App is listening on port ${process.env.PORT || 3000}.`)
 );
+
+module.exports = app;
