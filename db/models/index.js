6eb0feaac5daa55b7b17b904d2eb459f96dc0752
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-04-17 10:22:41 +0200

    feat: added db connection setup file

---
+++ db/models/index.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+const User = require('./user');
+const Expense = require('./expense');
+
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: console.log,
+    ssl: false,
+  }
+);
+
+sequelize
+  .getQueryInterface()
+  .sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
+
+const db = {
+  User: User(sequelize, Sequelize.DataTypes),
+  Expense: Expense(sequelize, Sequelize.DataTypes),
+};
+
+Object.keys(db).forEach((model) => {
+  if ('associate' in db[model]) {
+    db[model].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
