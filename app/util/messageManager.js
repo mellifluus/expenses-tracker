0488493decc3a3d2a858476d7904ba5a6079d74f
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-04-21 21:06:18 +0200

    feat: setup a way to persist messages using session

--- app/util/messageManager.js
+++ app/util/messageManager.js
@@ -1,3 +1,9 @@
+exports.storeMessages = (req, messages) => {
+  for (const [id, value] of Object.entries(messages)) {
+    (req.session.messages ??= {})[id] = value;
+  }
+};
+
 exports.flashMessages = (req, res, next) => {
   if (req.session.messages) {
     for (const [id, value] of Object.entries(req.session.messages)) {
