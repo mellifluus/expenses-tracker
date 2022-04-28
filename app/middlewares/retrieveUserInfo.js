c4c95523c9b2ac2388adb03dfde26e6783bc8be2
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-04-28 15:28:55 +0200

    setting isAuthenticated in retrieveuserinfo

--- app/middlewares/retrieveUserInfo.js
+++ app/middlewares/retrieveUserInfo.js
@@ -1,6 +1,9 @@
 exports.retrieveUserInfo = (req, res, next) => {
   if (req.session.user) {
+    res.locals.isAuthenticated = true;
     res.locals.user = req.session.user;
+  } else {
+    res.locals.isAuthenticated = false;
   }
   return next();
 };
