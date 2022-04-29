70aa7826cbe42e2d950c415b4aec765829514c79
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-04-29 11:53:58 +0200

    homepage almost ready to receive data from backend

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,10 +1,13 @@
 const getExpenses = (param) => {
   const xhr = new XMLHttpRequest();
-  xhr.open('POST', '/test', true);
+  xhr.open('POST', '/test', true); // change endpoint
   xhr.send();
-  xhr.onreadystatechange = function () {
+  xhr.onreadystatechange = () => {
     if (xhr.readyState == XMLHttpRequest.DONE) {
       console.log(xhr.responseText);
+      // apply data to chart
     }
   };
 };
+
+const chart = (document.querySelector('#chart'), {});
