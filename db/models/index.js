7d2f77fc6b19c787efd151a79ae5dd31db9f9e53
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-04-29 23:14:08 +0200

    retrying heroku

--- db/models/index.js
+++ db/models/index.js
@@ -1,37 +1,47 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
-const User = require('./user');
-const Expense = require('./expense');
-const Session = require('./session');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-require('dotenv').config();
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: console.log,
-    ssl: false,
-  }
-);
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 
 // To make sure the interface to generate UUID exists
 sequelize
   .getQueryInterface()
   .sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
 
-const db = {
-  User: User(sequelize, Sequelize.DataTypes),
-  Expense: Expense(sequelize, Sequelize.DataTypes),
-  Session: Session(sequelize, Sequelize.DataTypes),
-};
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
 
-Object.keys(db).forEach((model) => {
-  if ('associate' in db[model]) {
-    db[model].associate(db);
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
   }
 });
 
