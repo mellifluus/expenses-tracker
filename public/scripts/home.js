b3170d5817b0e68b54170aca34c83fcd27c667c4
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-04-29 14:50:56 +0200

    ready to apply data to chart

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,13 +1,40 @@
-const getExpenses = (param) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', '/test', true); // change endpoint
-  xhr.send();
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-      console.log(xhr.responseText);
-      // apply data to chart
-    }
-  };
+const selectYear = document.querySelector('#year');
+const selectMonth = document.querySelector('#month');
+
+const toggleSelects = () => {
+  if (
+    !selectYear.hasAttribute('disabled') &&
+    !selectMonth.hasAttribute('disabled')
+  ) {
+    selectYear.setAttribute('disabled', '');
+    selectMonth.setAttribute('disabled', '');
+  } else {
+    selectYear.removeAttribute('disabled');
+    selectMonth.removeAttribute('disabled');
+  }
+};
+
+const getExpenses = async () => {
+  toggleSelects();
+
+  fetch('/expense/get', {
+    method: 'POST',
+    body: new URLSearchParams({
+      year: selectYear.value || undefined,
+      month: selectMonth.value || undefined,
+    }),
+  })
+    .then((res) => {
+      if (res.status !== 200) {
+        throw new Error('Something went wrong with the request.');
+      }
+      return res.json();
+    })
+    .then((data) => {
+      //data is an array of expenses, do stuff with them
+    })
+    .catch((err) => console.error(err))
+    .finally(() => toggleSelects());
 };
 
 const chart = (document.querySelector('#chart'), {});
