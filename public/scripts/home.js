a6b448ee90abc88b8bea76f83f430b3965b19db4
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-04-29 10:03:54 +0200

    sketched out a function to get expenses from backend

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,38 +1,10 @@
-const ctx = document.getElementById('line-chart');
-const myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
-});
+const getExpenses = (param) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/test', true);
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      console.log(xhr.responseText);
+    }
+  };
+};
